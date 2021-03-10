949a9c0220f0197566d2ad7c53d249623d3b7c98
Nogueira <anogueirajr@gmail.com>
2021-03-10 09:41:26 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _20210310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste no Console!");
+            Console.ReadLine();
+        }
+    }
+}
